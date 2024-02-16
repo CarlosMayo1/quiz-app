c563ef7c08bb9218813aaa7162c55b92aa8a937b
CarlosMayo1 <carloshmayo@gmail.com>
2024-02-16 10:12:37 -0500

    rendering userModal conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Quiz from './componets/Quiz/Quiz'
 import UserModal from './componets/userModal/UserModal'
 function App() {
+	const [username, setUsername] = useState('')
 	const [isOpen, setIsOpen] = useState(true)
 
 	const closeModal = () => {
@@ -11,12 +12,21 @@ function App() {
 	}
 
 	return (
-		<>
-			{isOpen && <UserModal isOpen={isOpen} closeModal={closeModal} />}
-			<div className='h-screen flex justify-center items-center'>
-				<Quiz />
-			</div>
-		</>
+		<div className='h-screen flex justify-center items-center'>
+			{isOpen ? (
+				<UserModal
+					isOpen={isOpen}
+					setIsOpen={setIsOpen}
+					closeModal={closeModal}
+					username={username}
+					setUsername={setUsername}
+				/>
+			) : (
+				<div>
+					<Quiz />
+				</div>
+			)}
+		</div>
 	)
 }
 
--- src/componets/userModal/UserModal.jsx
+++ src/componets/userModal/UserModal.jsx
@@ -1,12 +1,45 @@
 //react
 import { useState, Fragment } from 'react'
+// react-hook-form
+import { useForm } from 'react-hook-form'
 // headless-ui
 import { Dialog, Transition } from '@headlessui/react'
 
-const UserModal = ({ isOpen, closeModal }) => {
+const UserModal = ({
+	isOpen,
+	setIsOpen,
+	closeModal,
+	username,
+	setUsername,
+}) => {
+	const {
+		register,
+		setError,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+
+	const handleSubmitForm = handleSubmit(data => {
+		setUsername(data.username)
+		setIsOpen(false)
+	})
+
+	const onCloseModal = () => {
+		if (username === '') {
+			console.log('working')
+			setError('username', {
+				type: 'custom',
+				message: 'This field cannot be empty!',
+			})
+			return
+		}
+
+		closeModal()
+	}
+
 	return (
 		<Transition appear show={isOpen} as={Fragment}>
-			<Dialog as='div' className='relative z-10' onClose={closeModal}>
+			<Dialog as='div' className='relative z-10' onClose={onCloseModal}>
 				<Transition.Child
 					as={Fragment}
 					enter='ease-out duration-300'
@@ -30,29 +63,37 @@ const UserModal = ({ isOpen, closeModal }) => {
 							leaveFrom='opacity-100 scale-100'
 							leaveTo='opacity-0 scale-95'
 						>
-							<Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
-								<Dialog.Title
-									as='h3'
-									className='text-lg font-medium leading-6 text-gray-900'
-								>
-									Payment successful
-								</Dialog.Title>
-								<div className='mt-2'>
-									<p className='text-sm text-gray-500'>
-										Your payment has been successfully submitted. We’ve sent you
-										an email with all of the details of your order.
-									</p>
-								</div>
-
-								<div className='mt-4'>
-									<button
-										type='button'
-										className='inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
-										onClick={closeModal}
-									>
-										Got it, thanks!
-									</button>
-								</div>
+							<Dialog.Panel className='w-full max-w-xs transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+								<form onSubmit={handleSubmitForm}>
+									<div className='mb-2'>
+										<input
+											type='text'
+											className='w-full border border-slate-300 py-1.5 px-2 rounded-md mb-1 outline-none focus:border-color-3'
+											placeholder='Username'
+											autoComplete='off'
+											id='username'
+											{...register('username', {
+												required: {
+													value: true,
+													message: 'This field cannot be empty!',
+												},
+											})}
+										/>
+										<div>
+											<p className='text-sm text-red-500'>
+												{errors.username && errors.username.message}
+											</p>
+										</div>
+									</div>
+									<div>
+										<button
+											type='submit'
+											className='w-full py-2 px-3 text-center text-white font-semibold bg-color-3 rounded-md'
+										>
+											Ingresar
+										</button>
+									</div>
+								</form>
 							</Dialog.Panel>
 						</Transition.Child>
 					</div>
